6c54f290421fb018862930b04e83152990797477
Michelle Brytowski <mn.brytowski@gmail.com>
2019-02-03 20:45:24 -0700

    fix connection

--- config/connection.js
+++ config/connection.js
@@ -11,19 +11,19 @@ var connection = mysql.createConnection({
 });
 
 // Make connection.
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
+
+// connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+//   if (err) throw err;
+
+//   console.log('The solution is: ', rows[0].solution);
+// });
 
 // Export connection for our ORM to use.
 module.exports = connection;
