e0b519902c33040971dbc170421e27a9b02ea837
Michelle Brytowski <mn.brytowski@gmail.com>
2019-02-03 20:17:52 -0700

    updates

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,28 @@
 var mysql = require("mysql");
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+connection.connect();
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "d13xat1hwxt21t45.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//   port: 3306,
+//   user: "upnzjiuzbn72a3vd",
+//   password: "mxd8shhzvvwc8l9w",
+//   database: "zr392agqgzqwu3xq"
+// });
 
 // Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
 });
 
 // Export connection for our ORM to use.
