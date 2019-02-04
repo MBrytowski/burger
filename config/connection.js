da9eff283d3f3cb35cdd36d748f322d6c146631b
Michelle Brytowski <mn.brytowski@gmail.com>
2019-02-03 21:09:55 -0700

    deploy issues

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,26 @@
 var mysql = require("mysql");
-// var connection = mysql.createConnection(process.env.JAWSDB_URL);
-// connection.connect();
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "zr392agqgzqwu3xq"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "zr392agqgzqwu3xq"
+  });
+}
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+  // Make connection.
+  connection.connect(function (err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
 
 // connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
 //   if (err) throw err;
